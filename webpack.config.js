e4f71b7077a8c69e21c11bc8a23453fcf7d52e24
Rico Chen <ricochenx@gmail.com>
2018-04-24 22:30:03 -0700

    create config files

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const webpack = require('webpack');
+
+module.exports = {
+    entry: [
+        'react-hot-loader/patch',
+        './src/js/index.js'
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    },
+    output: {
+        path: __dirname + '/dist',
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    devServer: {
+        contentBase: './dist',
+        port: 8545,
+        hot: true
+    }
+};
