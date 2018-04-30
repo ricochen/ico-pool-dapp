b005d4fe13ab8113e0f863ba9e9a33e12e0f6339
Rico Chen <ricochenx@gmail.com>
2018-04-29 21:43:27 -0700

    create crowdsale contract

---
+++ src/js/components/Crowdsale.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Crowdsale extends React.Component {
+    render() {
+        return (
+            <div className="Crowdsale">
+                Crowdsale
+            </div>
+        )
+    }
+}
+
+export default Crowdsale
