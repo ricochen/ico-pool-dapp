2de9ffa8e37ccc7d934648703a8657ae4800a6bf
Rico Chen <ricochenx@gmail.com>
2018-05-07 23:51:05 -0700

    add rinkeby network

--- truffle.js
+++ truffle.js
@@ -5,6 +5,14 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      // geth attach \\\\.\\pipe\\geth.ipc
+      // geth --rinkeby --rpc --rpcapi db,eth,net,web3,personal --unlock="0x87584f1E69f6233cd18B502660CBea4312CBA5A9" --rpccorsdomain http://localhost:8545
+      host: "localhost",
+      port: 8545,
+      network_id: "4", // Rinkeby ID 4
+      from: "0x422baffe21d13d76af304a09d24b284d4b40bb39" // account from which to deploy
     }
   },
   solc: {
